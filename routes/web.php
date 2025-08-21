604bbba3dcf6152eac89736942931299aa230dca
Alex Karolik <akarolik@gmail.com>
2025-08-21 11:44:51 +0000

    added Pages as Twill module

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-// локализованные маршруты
-Route::group([
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => [
-        'localize',
-        'localizationRedirect',
-        'localeViewPath',
-        // session/cookie редиректы убраны, чтобы / всегда был EN
-    ],
-], function () {
-    Route::get('/', fn () => view('home'))->name('home');
-    Route::get('/about', fn () => view('about'))->name('about');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\PageDisplayController::class, 'home'])->name('frontend.home');
+Route::get('{slug}', [\App\Http\Controllers\PageDisplayController::class, 'show'])->name('frontend.page');
